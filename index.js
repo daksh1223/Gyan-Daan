748c44795b2126c56048af2ab2e7b85249656134
daksh1223 <70289051+daksh1223@users.noreply.github.com>
2022-01-24 19:34:15 +0530

    Revert "Fuix File upload issues"
    This reverts commit 6ef22fb7a6829378d257ed320db068a000fefe68.

--- index.js
+++ index.js
@@ -216,7 +216,7 @@ app.post("/api/uploadFile", upload.single('upload'), async (req, res) => {
     if(req.body.channelID)
     {
       const channel = await find_channel_by_id(req.body.channelID)
-      if (req.body.isRecording==='true')
+      if (req.body.isRecording)
         channel.recordings.push(newFile)
       else 
         channel.files.push(newFile)
@@ -226,7 +226,7 @@ app.post("/api/uploadFile", upload.single('upload'), async (req, res) => {
     res.send(newFile);
   } catch (error) {
     res.status(500).json({
-      error
+      error,
     });
   }
 });
